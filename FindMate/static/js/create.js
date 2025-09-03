a4684a6d0061d71a9640d62b03286f3b94b46ed6
JO HANHEE <hh750715@gmail.com>
2025-09-03 11:42:51 +0900

    feat#26 create.html 구현

---
+++ FindMate/static/js/create.js
@@ -0,0 +1,12 @@
+function clickCreateBtn() {
+  title = document.getElementById("study-title").value;
+  host = document.getElementById("study-host").value;
+  date = document.getElementById("study-date").value;
+  duration = document.getElementById("study-duration").value;
+  capacity = document.getElementById("study-capacity").value;
+  description = document.getElementById("study-description").value;
+
+  // 서버로 데이터 전송
+}
+
+
